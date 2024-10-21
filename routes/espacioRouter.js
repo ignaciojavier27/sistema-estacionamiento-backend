b13c626a03683c00bfe0826684706adba1ebb223
ignaciojavier27 <nacholin124@gmail.com>
2024-10-20 21:06:45 -0300

    Espacios controlador

--- routes/espacioRouter.js
+++ routes/espacioRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { obtenerEspacios, obtenerEspacioPorId, obtenerEspaciosPorPropietario } from '../controllers/espacioController.js';
+import { obtenerEspacios, obtenerEspacioPorId, obtenerEspaciosPorPropietario, obtenerEspaciosPorEstacionamiento } from '../controllers/espacioController.js';
 
 const router = express.Router();
 
 router.get('/espacios', obtenerEspacios);
 router.get('/espacios/:id', obtenerEspacioPorId);
 router.get('/espacios/propietario/:propietario_id', obtenerEspaciosPorPropietario);
+router.get('/espacios/estacionamiento/:estacionamiento_id', obtenerEspaciosPorPropietario);
+
 
 export default router;
