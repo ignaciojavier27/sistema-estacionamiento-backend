f2ebcbf8d013c4b1be1e66e2835819454ce27b92
ignaciojavier27 <nacholin124@gmail.com>
2024-10-22 13:53:58 -0300

    Modificaciones controller ingreso vehiculo

--- controllers/ingresoVehiculoController.js
+++ controllers/ingresoVehiculoController.js
@@ -1,26 +1,29 @@
 import Espacio from '../models/Espacio.js';
 import IngresoVehiculo from '../models/IngresoVehiculo.js';
 export const registrarIngresoVehiculo = async (req, res) => {
-    try {
+  try {
       const { espacio_id, patente } = req.body;
       
       const espacio = await Espacio.findByPk(espacio_id);
       if (!espacio) {
-        return res.status(404).json({ message: 'Espacio no encontrado' });
+          return res.status(404).json({ message: 'Espacio no encontrado' });
       }
       if (espacio.estado === 1) {
-        return res.status(400).json({ message: 'Espacio ya está ocupado' });
+          return res.status(400).json({ message: 'Espacio ya está ocupado' });
       }
-      
+
+      // Crear el ingreso del vehículo
+      const ingreso = await IngresoVehiculo.create({ patente, espacio_id });
+
       espacio.estado = 1;
+      espacio.ingreso_id = ingreso.ingreso_id;
+      espacio.patente = patente;
       await espacio.save();
       
-      const ingreso = await IngresoVehiculo.create({ patente, espacio_id });
-      
       res.status(200).json({ message: 'Vehículo ingresado correctamente', ingreso });
-    } catch (error) {
+  } catch (error) {
       res.status(500).json({ message: 'Error al registrar el ingreso', error: error.message });
-    }
+  }
 };
 
 export const obtenerIngresoVehiculos = async (req, res) => {
